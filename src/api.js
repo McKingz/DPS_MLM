48335658fbe8cfa4a74b1d1d23d331f41bf29cea
K1NG-Devops <61783617@mylife.unisa.ac.za>
2024-06-05 01:04:07 +0200

    modified src/api.js

--- src/api.js
+++ src/api.js
@@ -1,13 +1,16 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
+const apiUrl = process.env.REACT_APP_API_URL;
 
-const apiClient = axios.create({
-    baseURL: API_BASE_URL,
-    headers: {
-        'Content-Type': 'application/json',
-    },
+fetch(`${apiUrl}/some-endpoint`, {
+  method: 'GET',
+  // other fetch options
+})
+.then(response => response.json())
+.then(data => {
+  // handle the data
 });
+
 
 export const registerUser = async (formData) => {
     const response = await axios.post(`${process.env.REACT_APP_API_URL}/register/`, formData, {
